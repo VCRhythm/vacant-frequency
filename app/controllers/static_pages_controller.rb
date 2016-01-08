6d91ace576c1e03f1b60c7ce8feb394f9331c292
Tristan Hardy <tthard@gmail.com>
2016-01-08 19:30:35 +0000

    footie

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,12 +8,16 @@ class StaticPagesController < ApplicationController
     @active_tab = 2
   end 
 
-  def the_pool
+  def footie
     @active_tab = 3
   end
 
-  def fivefourthreetwoone
+  def the_pool
     @active_tab = 4
+  end
+
+  def fivefourthreetwoone
+    @active_tab = 5
   end
 
 end
